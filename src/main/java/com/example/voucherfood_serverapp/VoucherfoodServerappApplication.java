467f8fd91a09ca39ee64f4eb0a108a60310f5bb9
Toto <totoabdulpatah45@gmail.com>
2025-09-20 06:13:32 +0700

    first commit

---
+++ src/main/java/com/example/voucherfood_serverapp/VoucherfoodServerappApplication.java
@@ -0,0 +1,13 @@
+package com.example.voucherfood_serverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherfoodServerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherfoodServerappApplication.class, args);
+	}
+
+}
